11157f868bfc1d3e52bcd85db586062a1998963f
yingyaozhou <yzhou238@uic.edu>
2023-01-07 21:29:36 -0600

    syntax

--- Lecture Notes/Lecture14.py
+++ Lecture Notes/Lecture14.py
@@ -145,12 +145,126 @@ def partition_tree(n, m):
         right = partition_tree(n, m - 1)
         return tree(m, [left, right])
 
-def print_parts(tree,partition = []):
+
+def print_parts(tree, partition=[]):
     if is_leaf(tree):
         if label(tree):
             print('+'.join(partition))
     else:
         left, right = branches(tree)
         m = str(label(tree))
-        print_parts(left, partition+[m])
-        print_parts(right,partition)
+        print_parts(left, partition + [m])
+        print_parts(right, partition)
+
+
+empty = 'empty'
+
+
+def is_link(s):
+    """s is a linked list if it is empty or a (first, rest) pair."""
+    return s == empty or (len(s) == 2 and is_link(s[1]))
+
+
+def link(first, rest):
+    """Construct a linked list from its first element and the rest."""
+    assert is_link(rest), "rest must be a linked list."
+    return [first, rest]
+
+
+def first(s):
+    """Return the first element of a linked list s."""
+    assert is_link(s), "first only applies to linked lists."
+    assert s != empty, "empty linked list has no first element."
+    return s[0]
+
+
+def rest(s):
+    """Return the rest of the elements of a linked list s."""
+    assert is_link(s), "rest only applies to linked lists."
+    assert s != empty, "empty linked list has no rest."
+    return s[1]
+
+
+def len_link(s):
+    """Return the length of linked list s."""
+    length = 0
+    while s != empty:
+        s, length = rest(s), length + 1
+    return length
+
+
+def getitem_link(s, i):
+    """Return the element at index i of linked list s."""
+    while i > 0:
+        s, i = rest(s), i - 1
+    return first(s)
+
+
+def len_link_recursive(s):
+    """Return the length of linked list s."""
+    if s == empty:
+        return 0
+    return 1 + len_link_recursive(rest(s))
+
+
+def getitem_link_recursive(s, i):
+    """Return the element at index i of linked list s."""
+    if i == 0:
+        return first(s)
+    return getitem_link(rest(s), i - 1)
+
+
+def extend_link(s, t):
+    """Return a list with the elements of s followed by those of t."""
+    assert is_link(s) and is_link(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+
+
+def apply_to_all_link(f, s):
+    """Apply f to each element of s."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        return link(f(first(s)), apply_to_all_link(f, rest()))
+
+
+def keep_if_link(f, s):
+    """Return a list with elements of s for which f(e) is true."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        kept = keep_if_link(f, rest(s))
+        if f(first(s)):
+            return link(first(s), kept)
+        else:
+            return kept
+
+
+def join_link(s, separator):
+    """Return a string of all elements in s separated by separator."""
+    if s == empty:
+        return ""
+    elif rest(s) == empty:
+        return str(first(s))
+    else:
+        return str(first(s)) + separator + join_link(rest(s), separator)
+
+
+def partitions(n, m):
+    """Return a linked list of partitions of n using parts of up to m.
+    Each partition is represented as a linked list.
+    """
+    if n == 0:
+        return link(empty, empty)
+    elif n < 0 or m == 0:
+        return empty
+    else:
+        using_m = partitions(n - m, m)
+        with_m = apply_to_all_link(lambda s: link(m, s), using_m)
+        without_m = partitions(n, m - 1)
+        return extend_link(with_m, without_m)
---
+++ discussion/discussion5.py
@@ -0,0 +1,183 @@
+def my_map(fn, seq):
+    """Applies fn onto each element in seq and returns a list.
+    >>> my_map(lambda x: x*x, [1, 2, 3])
+    [1, 4, 9]
+    """
+    "*** YOUR CODE HERE ***"
+    return [fn(ele) for ele in seq]
+
+
+def my_filter(pred, seq):
+    """Keeps elements in seq only if they satisfy pred.
+    >>> my_filter(lambda x: x % 2 == 0, [1, 2, 3, 4])  # new list has only even-valued elements
+    [2, 4]
+    """
+    "*** YOUR CODE HERE ***"
+    return [ele for ele in seq if pred(ele)]
+
+
+def my_reduce(combiner, seq):
+    """Combines elements in seq using combiner.
+    seq will have at least one element.
+    >>> my_reduce(lambda x, y: x + y, [1, 2, 3, 4])  # 1 + 2 + 3 + 4
+    10
+    >>> my_reduce(lambda x, y: x * y, [1, 2, 3, 4])  # 1 * 2 * 3 * 4
+    24
+    >>> my_reduce(lambda x, y: x * y, [4])
+    4
+    >>> my_reduce(lambda x, y: x + 2 * y, [1, 2, 3]) # (1 + 2 * 2) + 2 * 3
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    result = seq[0]
+    for ele in seq[1:]:
+        result = combiner(result, ele)
+    return result
+
+
+def count_palindromes(L):
+    """The number of palindromic strings in the sequence of strings
+    L (ignoring case).
+    >>> count_palindromes(("Acme", "Madam", "Pivot", "Pip"))
+    2
+    >>> count_palindromes(["101", "rAcECaR", "much", "wow"])
+    3
+    """
+    return len(my_filter(lambda x: x.lower() == x[::-1].lower(), L))
+
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    >>> t = tree(3, [tree(1), tree(2, [tree(5, [tree(6)]), tree(1)])])
+    >>> height(t)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return 0
+    return 1 + max([height(b) for b in branches(t)])
+
+
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+    return label(t) + max([max_path_sum(b) for b in branches(t)])
+
+
+def find_path(t, x):
+    """
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if label(t) == x:
+        return [label(t)]
+    paths = [find_path(b, x) for b in branches(t)]
+    for path in paths:
+        if path:
+            return [label(t)] + path
+
+
+def sum_tree(t):
+    """
+    Add all elements in a tree.
+    >>> t = tree(4, [tree(2, [tree(3)]), tree(6)])
+    >>> sum_tree(t)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+    # if is_leaf(t):
+    #     return label(t)
+    return label(t) + sum([sum_tree(b) for b in branches(t)])
+
+
+def balanced(t):
+    """
+    Checks if each branch has same sum of all elements and
+    if each branch is balanced.
+    >>> t = tree(1, [tree(3), tree(1, [tree(2)]), tree(1, [tree(1), tree(1)])])
+    >>> balanced(t)
+    True
+    >>> t = tree(1, [t, tree(1)])
+    >>> balanced(t)
+    False
+    >>> t = tree(1, [tree(4), tree(1, [tree(2), tree(1)]), tree(1, [tree(3)])])
+    >>> balanced(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    # for b in branches(t):
+    #     if sum_tree(b) != sum_tree(branches(t)[0]) or not balanced(b):
+    #         return False
+    # return True
+
+    return all([False if sum_tree(b) != sum_tree(branches(t)[0]) or not balanced(b) else True for b in branches(t)])
+
+
+def hailstone_tree(n, h):
+    """Generates a tree of hailstone numbers that will reach N, with height H.
+    >>> print_tree(hailstone_tree(1, 0))
+    1
+    >>> print_tree(hailstone_tree(1, 4))
+    1
+        2
+            4
+                8
+                    16
+    >>> print_tree(hailstone_tree(8, 3))
+    8
+        16
+            32
+                64
+            5
+                10
+    """
+    if h == 0:
+        return tree(n)
+    branches = [hailstone_tree(n * 2, h - 1)]
+    if (n - 1) % 3 == 0 and ((n - 1) // 3) % 2 == 1 and (n - 1) // 3 > 1:
+        branches += [hailstone_tree((n - 1) // 3, h - 1)]
+    return tree(n, branches)
+
+
+def print_tree(t):
+    def helper(i, t):
+        print("    " * i + str(label(t)))
+        for b in branches(t):
+            helper(i + 1, b)
+
+    helper(0, t)
