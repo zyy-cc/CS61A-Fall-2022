489c032d8fe5b04a3c67d96506a9b80fb38533e0
yingyaozhou <yzhou238@uic.edu>
2023-01-14 11:25:10 -0600

    week7

--- Homework/hw08/hw08.scm
+++
@@ -1,8 +0,0 @@
-(define (my-filter pred s) 'YOUR-CODE-HERE)
-
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
-
-(define (accumulate joiner start n term)
-  'YOUR-CODE-HERE)
-
-(define (no-repeats lst) 'YOUR-CODE-HERE)
--- Homework/hw09/hw09.scm
+++
@@ -1,8 +0,0 @@
-(define-macro (when condition exprs)
-  'YOUR-CODE-HERE)
-
-(define-macro (switch expr cases)
-  (cons _________
-        (map (_________ (_________)
-                        (cons _________ (cdr case)))
-             cases)))
