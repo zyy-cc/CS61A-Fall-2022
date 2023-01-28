270d8ce22fa7c0823e13161a2cce49895dbaa259
yingyaozhou <yzhou238@uic.edu>
2023-01-27 22:08:40 -0600

    dis7

---
+++ discussion/discussion7.py
@@ -0,0 +1,197 @@
+class Email:
+    """Every email object has 3 instance attributes: the
+    message, the sender name, and the recipient name.
+    >>> email = Email('hello', 'Alice', 'Bob')
+    >>> email.msg
+    'hello'
+    >>> email.sender_name
+    'Alice'
+    >>> email.recipient_name
+    'Bob'
+    """
+
+    def __init__(self, msg, sender_name, recipient_name):
+        "*** YOUR CODE HERE ***"
+        self.msg = msg
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
+
+
+class Server:
+    """Each Server has an instance attribute clients, which
+    is a dictionary that associates client names with
+    client objects.
+    """
+
+    def __init__(self):
+        self.clients = {}
+
+    def send(self, email):
+        """Take an email and put it in the inbox of the client
+        it is addressed to.
+        """
+        "*** YOUR CODE HERE ***"
+        client = self.clients[email.recipient_name]
+        client.receive(email)
+
+    def register_client(self, client, client_name):
+        """Takes a client object and client_name and adds them
+        to the clients instance attribute.
+        """
+        "*** YOUR CODE HERE ***"
+        self.clients[client_name] = client
+
+
+class Client:
+    """Every Client has instance attributes name (which is
+    used for addressing emails to the client), server
+    (which is used to send emails out to other clients), and
+    inbox (a list of all emails the client has received).
+
+    >>> s = Server()
+    >>> a = Client(s, 'Alice')
+    >>> b = Client(s, 'Bob')
+    >>> a.compose('Hello, World!', 'Bob')
+    >>> b.inbox[0].msg
+    'Hello, World!'
+    >>> a.compose('CS 61A Rocks!', 'Bob')
+    >>> len(b.inbox)
+    2
+    >>> b.inbox[1].msg
+    'CS 61A Rocks!'
+    """
+
+    def __init__(self, server, name):
+        self.inbox = []
+        "*** YOUR CODE HERE ***"
+        self.server = server
+        self.name = name
+        self.server.register_client(self, self.name)
+
+    def compose(self, msg, recipient_name):
+        """Send an email with the given message msg to the
+        given recipient client.
+        """
+        "*** YOUR CODE HERE ***"
+        email = Email(msg, self.name, recipient_name)
+        self.server.send(email)
+
+    def receive(self, email):
+        """Take an email and add it to the inbox of this
+        client.
+        """
+        "*** YOUR CODE HERE ***"
+
+        self.inbox.append(email)
+
+
+class Pet:
+
+    def __init__(self, name, owner):
+        self.is_alive = True  # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+
+class Dog(Pet):
+
+    def talk(self):
+        super().talk()
+        print('This Dog says woof!')
+
+
+class Cat(Pet):
+
+    def __init__(self, name, owner, lives=9):
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner)
+        self.lives = lives
+
+    def talk(self):
+        """Print out a cat's greeting.
+
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        print("{} says meow!".format(self.name))
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero,
+        is_alive becomes False. If this is called after lives has
+        reached zero, print 'This cat has no more lives to lose.'
+        """
+        "*** YOUR CODE HERE ***"
+        if not self.is_alive:
+            print('This cat has no more lives to lose.')
+        else:
+            self.lives -= 1
+            if self.lives == 0:
+                self.is_alive = False
+
+
+class NoisyCat(Cat):  # Fill me in!
+    """A Cat that repeats things twice."""
+
+    def __init__(self, name, owner, lives=9):
+        # Is this method necessary? Why or why not?
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner, lives)
+
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        for _ in range(2):
+            super().talk()
+
+
+class Dog(Pet):
+    # With the previously defined methods not written out
+    @classmethod
+    def robo_factory(cls, owner):
+        return cls("RoboDog", owner)
+
+
+class Cat:
+    def __init__(self, name, owner, lives=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        return self.name + ' says meow!'
+
+    @classmethod
+    def cat_creator(cls, owner):
+        """
+        Returns a new instance of a Cat.
+
+        This instance's name is "[owner]'s Cat", with
+        [owner] being the name of its owner.
+
+        >>> cat1 = Cat.cat_creator("Bryce")
+        >>> isinstance(cat1, Cat)
+        True
+        >>> cat1.owner
+        'Bryce'
+        >>> cat1.name
+        "Bryce's Cat"
+        >>> cat2 = Cat.cat_creator("Tyler")
+        >>> cat2.owner
+        'Tyler'
+        >>> cat2.name
+        "Tyler's Cat"
+        """
+        name = owner + "'s Cat"
+        return cls(name, owner)
